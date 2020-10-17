e83baea3b125393c8cd1552f19970d86c84aedf0
Marko Bencun <marko@shiftcrypto.ch>
2020-10-17 23:25:47 +0200

    wip

--- src/rust/bitbox02-rust-c/src/async_usb.rs
+++ src/rust/bitbox02-rust-c/src/async_usb.rs
@@ -52,7 +52,7 @@ pub unsafe extern "C" fn rust_async_usb_copy_response(out: *mut bitbox02::buffer
 pub extern "C" fn rust_async_usb_spawn_hww(usb_in: crate::util::Bytes) {
     use bitbox02_rust::async_usb::spawn;
     use bitbox02_rust::hww::process_packet;
-    spawn(process_packet, &usb_in.as_ref());
+    spawn(usb_in.as_ref().to_vec());
 }
 
 #[no_mangle]
--- src/rust/bitbox02-rust/src/async_usb.rs
+++ src/rust/bitbox02-rust/src/async_usb.rs
@@ -17,11 +17,12 @@
 
 extern crate alloc;
 
-use crate::bb02_async::{spin as spin_task, Task};
+use crate::bb02_async::{option, spin as spin_task, AsyncOption, Task};
 use alloc::boxed::Box;
 use alloc::vec::Vec;
 use core::cell::RefCell;
-use core::task::Poll;
+use core::pin::Pin;
+use core::task::{Context, Poll};
 
 type UsbOut = Vec<u8>;
 type UsbIn = Vec<u8>;
@@ -53,27 +54,78 @@ struct SafeUsbTaskState(RefCell<UsbTaskState<'static>>);
 /// run only in a single thread in the BitBox02.
 unsafe impl Sync for SafeUsbTaskState {}
 
+struct BeSafe<A>(A);
+unsafe impl<A> Sync for BeSafe<A> {}
+
 /// Executor main state. Currently we only have at most one task at a time (usb api processing
 /// task).
 static USB_TASK_STATE: SafeUsbTaskState = SafeUsbTaskState(RefCell::new(UsbTaskState::Nothing));
+
+enum OutFuture {
+    ResultAvailable(UsbOut),
+    ResultFetched,
+}
+
+struct OutFutureCell<'a>(&'a RefCell<OutFuture>);
+impl core::future::Future for OutFutureCell<'_> {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match *self.0.borrow() {
+            OutFuture::ResultAvailable(_) => Poll::Pending,
+            OutFuture::ResultFetched => Poll::Ready(()),
+        }
+    }
+}
+
+static IN_FUTURE: BeSafe<RefCell<Option<UsbIn>>> = BeSafe(RefCell::new(None));
+static OUT_FUTURE: BeSafe<RefCell<OutFuture>> = BeSafe(RefCell::new(OutFuture::ResultFetched));
+
+static mut RUNNING: bool = false;
+
+pub async fn usb_in() -> UsbIn {
+    option(&IN_FUTURE.0).await;
+    unsafe {
+        RUNNING = true;
+    }
+    IN_FUTURE.0.borrow_mut().take().unwrap()
+}
+
+pub async fn hww_main_loop() {
+    loop {
+        let in_bytes = usb_in().await;
+        let out_bytes = crate::hww::process_packet(in_bytes).await;
+        usb_out(out_bytes).await;
+    }
+}
+
+static mut MEGALOOP_TASK: Option<Task<()>> = None;
 
 /// Spawn a task to be spinned by the executor. This moves the state
 /// from Nothing to Running.
 ///
 /// *panics* - can only be called if the state is Nothing, otherwise panics.
-pub fn spawn<F>(workflow: fn(UsbIn) -> F, usb_in: &[u8])
-where
-    F: core::future::Future<Output = UsbOut> + 'static,
-{
-    let mut state = USB_TASK_STATE.0.borrow_mut();
+pub fn spawn(usb_in: UsbIn) {
+    let mut state = IN_FUTURE.0.borrow_mut();
     match *state {
-        UsbTaskState::Nothing => {
-            let task: Task<UsbOut> = Box::pin(workflow(usb_in.to_vec()));
-
-            *state = UsbTaskState::Running(Some(task));
-        }
-        _ => panic!("previous task still in progress"),
+        None => *state = Some(usb_in.to_vec()),
+        Some(_) => panic!("previous task still in progress"),
     }
+}
+
+pub async fn usb_out(result: UsbOut) {
+    {
+        let mut state = OUT_FUTURE.0.borrow_mut();
+        match &*state {
+            ResultFetched => {
+                *state = OutFuture::ResultAvailable(result);
+                unsafe {
+                    RUNNING = false;
+                }
+            }
+            OutFuture::ResultAvailable(_) => panic!("already sending result"),
+        }
+    }
+    OutFutureCell(&OUT_FUTURE.0).await
 }
 
 /// Spin the currently running task, if there is one. Otherwise do
@@ -82,31 +134,39 @@ where
 /// If this spin finishes the task, the state is moved to
 /// `ResultAvailable`, which contains the result.
 pub fn spin() {
-    // Pop task before polling, so that USB_TASK_STATE does not stay borrowed during the poll.
-    let mut popped_task = match *USB_TASK_STATE.0.borrow_mut() {
-        // Illegal state, `None` is only valid during the poll.
-        UsbTaskState::Running(None) => panic!("task not found"),
-        // Get the task out, putting `None` in. This allows us to release the mutable borrow on the
-        // state.
-        UsbTaskState::Running(ref mut task @ Some(_)) => task.take(),
-        // Nothing to do.
-        _ => None,
-    };
-    if let Some(ref mut task) = popped_task {
-        match spin_task(task) {
-            Poll::Ready(result) => {
-                *USB_TASK_STATE.0.borrow_mut() = UsbTaskState::ResultAvailable(result);
-            }
-            Poll::Pending => {
-                // Not done yet, put the task back for execution.
-                if let UsbTaskState::Running(ref mut task @ None) = *USB_TASK_STATE.0.borrow_mut() {
-                    *task = popped_task;
-                } else {
-                    panic!("spin: illegal executor state");
-                }
-            }
+    match *unsafe { &mut MEGALOOP_TASK } {
+        None => {
+            unsafe { MEGALOOP_TASK = Some(Box::pin(hww_main_loop())) };
+        }
+        Some(ref mut t) => {
+            spin_task(t);
         }
     }
+    // // Pop task before polling, so that USB_TASK_STATE does not stay borrowed during the poll.
+    // let mut popped_task = match *USB_TASK_STATE.0.borrow_mut() {
+    //     // Illegal state, `None` is only valid during the poll.
+    //     UsbTaskState::Running(None) => panic!("task not found"),
+    //     // Get the task out, putting `None` in. This allows us to release the mutable borrow on the
+    //     // state.
+    //     UsbTaskState::Running(ref mut task @ Some(_)) => task.take(),
+    //     // Nothing to do.
+    //     _ => None,
+    // };
+    // if let Some(ref mut task) = popped_task {
+    //     match spin_task(task) {
+    //         Poll::Ready(result) => {
+    //             *USB_TASK_STATE.0.borrow_mut() = UsbTaskState::ResultAvailable(result);
+    //         }
+    //         Poll::Pending => {
+    //             // Not done yet, put the task back for execution.
+    //             if let UsbTaskState::Running(ref mut task @ None) = *USB_TASK_STATE.0.borrow_mut() {
+    //                 *task = popped_task;
+    //             } else {
+    //                 panic!("spin: illegal executor state");
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 #[derive(PartialEq, Debug)]
@@ -126,28 +186,31 @@ pub enum CopyResponseErr {
 /// pending and a response is expected in the future, or `Err(false)`
 /// if no task is running.
 pub fn copy_response(dst: &mut [u8]) -> Result<usize, CopyResponseErr> {
-    let mut state = USB_TASK_STATE.0.borrow_mut();
+    if unsafe { RUNNING } {
+        return Err(CopyResponseErr::NotReady);
+    }
+    let mut state = OUT_FUTURE.0.borrow_mut();
     match *state {
-        UsbTaskState::Nothing => Err(CopyResponseErr::NotRunning),
-        UsbTaskState::Running(_) => Err(CopyResponseErr::NotReady),
-        UsbTaskState::ResultAvailable(ref response) => {
+        OutFuture::ResultAvailable(ref response) => {
             let len = response.len();
             dst[..len].copy_from_slice(&response.as_ref());
-            *state = UsbTaskState::Nothing;
+            *state = OutFuture::ResultFetched;
             Ok(len)
         }
+        OutFuture::ResultFetched => Err(CopyResponseErr::NotRunning),
     }
 }
 
 /// Cancel and drop a running task. Returns true if a task was cancelled, false if no task was
 /// running.
 pub fn cancel() -> bool {
-    let mut state = USB_TASK_STATE.0.borrow_mut();
-    if let UsbTaskState::Running(_) = *state {
-        *state = UsbTaskState::Nothing;
-        return true;
+    match *unsafe { &mut MEGALOOP_TASK } {
+        None => false,
+        Some(_) => {
+            unsafe { MEGALOOP_TASK = Some(Box::pin(hww_main_loop())) };
+            true
+        }
     }
-    false
 }
 
 #[cfg(test)]
