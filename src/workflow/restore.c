d66f76cfa885851ff883849e94207de5038c8a11
Marko Bencun <marko@shiftcrypto.ch>
2020-12-31 17:02:29 +0100

    restore: inline restore_seed()
    It is weird that this function also sets the birthdate, but not the
    backup name. They can be next to each other in the restore
    workflow. Also one less function to wrap in Rust.

--- src/workflow/restore.c
+++ src/workflow/restore.c
@@ -50,25 +50,32 @@ bool workflow_restore_backup(const RestoreBackupRequest* restore_request)
         return false;
     }
 
-    if (!restore_seed(&backup_data, password)) {
-        workflow_status_blocking("Could not\nrestore backup", false);
+    if (!keystore_encrypt_and_store_seed(backup_data.seed, backup_data.seed_length, password)) {
         return false;
     }
+
 #if APP_U2F == 1
     if (!securechip_u2f_counter_set(restore_request->timestamp)) {
         // ignore error
     }
 #endif
+
+    if (!memory_set_seed_birthdate(backup_data.birthdate)) {
+        // Ignore error here. Missing birthdate should not abort an otherwise successful
+        // restore.
+    }
+    if (!memory_set_device_name(backup.backup_v1.content.metadata.name)) {
+        // Ignore error here as well.
+    }
+
     if (!memory_set_initialized()) {
         return false;
     }
+
     uint8_t remaining_attempts;
     if (keystore_unlock(password, &remaining_attempts) != KEYSTORE_OK) {
         // This should/can never happen, but let's check anyway.
         Abort("workflow_restore_backup: unlock failed");
-    }
-    if (!memory_set_device_name(backup.backup_v1.content.metadata.name)) {
-        /* Ignore errors for now */
     }
     workflow_unlock_bip39_blocking();
     return true;
