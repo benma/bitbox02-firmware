0382db50b857144ad148df840c53726079db76e8
Marko Bencun <marko@shiftcrypto.ch>
2025-04-22 15:47:48 +0200

    memory_spi: add docstring

--- src/memory/memory_spi.h
+++ src/memory/memory_spi.h
@@ -36,6 +36,16 @@
 #error "Address must be aligned to an erase sector"
 #endif
 
+/**
+ * Retrieve the BLE firmware and associated size and checksum from the SPI memory chip. It takes
+ * into account the currently active firmware area, and verifies that it matches the
+ * `ble_allowed_firmware_hash` stored in the MCU shared memory.
+ * @param[out] firmware_out, if not NULL, will be set to a pointer to the heap of size `size_out`.
+ *            The caller must take care to `free()` it. Can be NULL.
+ * @param[out] size_out size of the loaded firmware.
+ * @param[out] checksum_out the checksum of the loaded firmware.
+ * @returns true on success, false on failure.
+ */
 USE_RESULT bool memory_spi_get_active_ble_firmware(
     uint8_t** firmware_out,
     size_t* size_out,
